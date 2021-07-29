ac2987a1f1a38d339427998f2dfcc3819acedd3a
JCBenR <jsieb81@gmail.com>
2021-07-29 15:36:05 -0600

    added datasets, imported datasets, made dropdown

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
 [State tax data](https://www.irs.gov/statistics/soi-tax-stats-individual-income-tax-state-data) **ONLY FROM 2018**
 
 [Tax data from congressional distric, country, zip](https://www.irs.gov/statistics/soi-tax-stats-data-by-geographic-area) **Downloaded the zip code file and it is MASSIVE**
+
+[Unemployement Rats 1/2020 thru 6/2021](https://www.bls.gov/charts/state-employment-and-unemployment/state-unemployment-rates-animated.htm)
--- newTest.js
+++ newTest.js
@@ -1,14 +1,68 @@
 let changeTitle = () => {
-    document.getElementById("Title").innerHTML= "New Stats";
+    document.getElementById("Title").innerHTML = "New Stats";
 }
 changeTitle();
 
-d3.csv("utah-history.csv")
-.then(res => {
-    console.log(res[0]);
-})
-.catch((error) => {
-    console.log(error);
-});
+// d3.csv("utah-history.csv")
+// .then(res => {
+//     console.log(res[0]);
+// })
+// .catch((error) => {
+//     console.log(error);
+// });
 
-utahCovidData();
+// Promise.all([
+//     d3.csv("utah-history.csv"),
+//     d3.csv("unemploymentByState.csv")
+// ]).then((files) => {
+//         var utahNums = files[0];
+//         var uempNums = files[1];
+
+//         for (const dp of data) {
+//             stateCovidNums.push(dp);
+//         }
+//         console.log(stateCovidNums);
+//         for (const dt of stateCovidNums) {
+//             console.log(`${dt.State} ${dt["Jan 2020"]}`);
+//         }
+//     })
+//     .catch((e) => {
+//         console.log(e);
+//     });
+
+
+    Promise.all([
+        d3.csv("utah-history.csv"),
+        d3.csv("unemploymentByState.csv")
+    ]).then((files) => {
+            var utahNums = files[0];
+            var uempNums = files[1];
+
+            /*GET LIST OF STATES */
+            var stateList = [];
+            for (const s of uempNums) {
+                stateList.push(s.State);
+            }
+            console.log(stateList);
+
+            /*CREATE DROP DOWN OF STATES */
+            let drp = document.createElement('select');
+            document.body.appendChild(drp);
+            stateList.forEach(e => {
+                var el = document.createElement('option');
+                el.textContent = e;
+                el.value = e;
+                drp.appendChild(el);
+            });
+
+            let ut = 'Utah';
+            let utahCovNums = uempNums.filter(obj => obj.State = ut)
+            console.log(utahCovNums[2]);
+            console.log(utahNums[3]);
+            // for (const dt of stateCovidNums) {
+            //     console.log(`${dt.State} ${dt["Jan 2020"]}`);
+            // }
+        })
+        .catch((e) => {
+            console.log(e);
+        });
