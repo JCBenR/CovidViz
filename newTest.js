bfca6475a8a7b8f7b2b746c017b68e3c5676a670
JCBenR <jsieb81@gmail.com>
2021-08-05 10:47:28 -0600

    getting stacks correctly

--- newTest.js
+++ newTest.js
@@ -3,7 +3,7 @@ let changeTitle = () => {
 }
 changeTitle();
 
-let months = ["Jan 2020", "Feb 2020", "Mar 2020", "Apr 2020", "May 2020", "Jun 2020", "Jul 2020", "Aug 2020", 
+let months = ["Jan 2020", "Feb 2020", "Mar 2020", "Apr 2020", "May 2020", "Jun 2020", "Jul 2020", "Aug 2020",
     "Sep 2020", "Oct 2020", "Nov 2020", "Dec 2020", "Jan 2021", "Feb 2021", "Mar 2021", "Apr 2021", "May 2021", "June 2021"];
 
 let stateAbrev = {
@@ -82,70 +82,13 @@ Promise.all([
     /*GET LIST OF STATES */
     covidData = data[0];
 
-    console.log(data[0][0]);
-
-
+    // console.log(data[0][0]);
     makeStateList();
     //var states = parseStates(covidData);
-    console.log(stateList);
+    // console.log(stateList);
 
 
-    var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
-
-
-// set the ranges
-//var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
-//var y = d3.scale.linear().range([height, 0]);
-
-// define the axis
-var xAxis = d3.svg.axis()
-    //.scale(x)
-    .orient("bottom")
-
-
-var yAxis = d3.svg.axis()
-   // .scale(y)
-    .orient("left")
-    .ticks(10);
-
-
-// add the SVG element
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
-
-// scale the range of the data
-x.domain(data.map(function(d) { return d[0][0].State; }));
-y.domain([0, d3.max(data, function(d) { return d[0][0].Population; })]);
-
-// add axis
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis)
-  .selectAll("text")
-    .style("text-anchor", "end")
-    .attr("dx", "-.8em")
-    .attr("dy", "-.55em")
-    .attr("transform", "rotate(-90)" );
-
-svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-  .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 5)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("Frequency");
-
-    //console.log(Object.keys(stateAbrev));
-let yearData = [];
+    let yearData = [];
     months.forEach(m => {
         let yrObj = {
             m: m,
@@ -155,9 +98,11 @@ let yearData = [];
             try {
                 let newObj = {
                     State: x.State,
-                    Pop: x.Population,
+                    ABBR: x.ABBR,
+                    Pop: parseInt(x.Population.replace(/,/g, '')),
                     Cases: x.data[m][0],
-                    Rate: x.data[m][1]
+                    Rate: x.data[m][1],
+                    UnEmp: Math.ceil(findEmpData(x.Population, x.data[m][1]))
                 }
                 yrObj.data.push(newObj);
                 // console.log(newObj);
@@ -165,23 +110,55 @@ let yearData = [];
                 // console.log(error);
             }
         }
-        yearData.push(yrObj); 
+        yearData.push(yrObj);
     })
+    // console.log(yearData.filter(obj => {return obj.m === month;})[0]);
     console.log(yearData);
-    // var dataset = d3.stack()(Object.keys(stateAbrev).map(function(newData) {
-        
-    //     return covidData.map(function(d) {
-    //         console.log(month);
-    //         console.log(d.data[month][0]);
-    //         //return {x: d.data[month][0], y: +d[newData]};
-    //     });
-    //     }));
+    const dataStack = d3.stack().keys(["Cases", "UnEmp", "Pop"]);
+    let sf = [];
+    yearData.forEach(e => {
+
+        let dsf = dataStack(e.data);
+        sf.push(dsf);
+    });
+
+    console.log(sf);
+    var margin = { top: 20, right: 20, bottom: 70, left: 40 },
+        width = 600 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+    var svg = d3.select("canvas")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var currYear = yearData.filter(obj => { return obj.m === month; })[0];
+    var x = d3.scaleBand()
+        .domain(currYear.ABBR)
+        .range([0, 100])
+
 })
     .catch((e) => {
-        console.error(e);
+        for (const x of Object.entries(e)) {
+            console.error(x);
+        }
+        //console.error(typeof e);
     });
 
 
+/**
+ * function transforms population from string to number, then changes rate to percent decimal (ie, 5.1 >> .0051), then multiplies the two to get the total number of unemployed in the state
+ * @param {*} pop population in string format
+ * @param {*} rate rate in decimal format (ie 5.1)
+ * @returns total number of unemployed in the state
+ */
+const findEmpData = (pop, rate) => {
+    pop = parseInt(pop.replace(/,/g, ''));
+    rate = rate / 100;
+    return pop * rate;
+}
 
 const makeStateList = () => {
     d3.select('#stateList').selectAll('p')
@@ -195,28 +172,28 @@ const makeStateList = () => {
         );
 }
 
-function parseStates(data){
+function parseStates(data) {
     var states = [];
-    for(let i = 0; i < 51; i++){
+    for (let i = 0; i < 51; i++) {
         states.push(data[i].State);
     }
     return states;
 }
 
-function parseUnemploymentRate(data, months){
+function parseUnemploymentRate(data, months) {
     var unemploymentRates = [];
-    for(let i = 0; i < 51; i++){
-        for(let j = 0; j < 18; j++){
+    for (let i = 0; i < 51; i++) {
+        for (let j = 0; j < 18; j++) {
             covidCases.push(data[months[j]]);
         }
     }
     return covidCases;
 }
 
-function parseCovidCases(data, months){
+function parseCovidCases(data, months) {
     var covidCases = {};
-    for(let i = 0; i < 51; i++){
-        for(let j = 0; j < 18; j++){
+    for (let i = 0; i < 51; i++) {
+        for (let j = 0; j < 18; j++) {
             covidCases.push(data[months[j]]);
 
         }
@@ -224,7 +201,7 @@ function parseCovidCases(data, months){
     return covidCases;
 }
 
-function handleClick(e, d){
+function handleClick(e, d) {
     let elem = d3.select(this); //wrap as a nice D3 object
     elem.classed('selected', !elem.classed('selected'));
 
@@ -257,13 +234,13 @@ let canvas = d3.select("#canvas")
 
 let bars = () => canvas.selectAll("rect")
 
-                .data(selectedStates)
-                .enter()
-                    .append("rect")
-                    .attr("width", 25)
-                    .attr("height", 200)
-                    .attr("fill", function(d) {return colorScale(d);}     
-                    )
+    .data(selectedStates)
+    .enter()
+    .append("rect")
+    .attr("width", 25)
+    .attr("height", 200)
+    .attr("fill", function (d) { return colorScale(d); }
+    )
 
 
 const colorScale = d3.scaleLinear()
@@ -306,11 +283,11 @@ var tooltip = d3.select("#canvas")
     .style("padding", "10px")
 
 
-var mouseover = function(d) {
+var mouseover = function (d) {
     tooltip.style("opacity", 1)
 }
 
-var mousemove = function(d) {
+var mousemove = function (d) {
     tooltip.html(`State: ${d.State} <br/>
                 Population: ${d.Population} <br/>
                 Cases: ${d.data[month][0]} <br/>
@@ -319,13 +296,13 @@ var mousemove = function(d) {
         //     "Population: " + d.Population + <br/> +
         //     "Cases: " + d.data[month][0] + <br/> +
         //     "UnEmp Rate: " + d.data[month][1]     
-      )
-      .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
-      .style("top", (d3.mouse(this)[1]) + "px")
-  }
+    )
+        .style("left", (d3.mouse(this)[0] + 90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+        .style("top", (d3.mouse(this)[1]) + "px")
+}
 
-  var mouseleave = function(d) {
+var mouseleave = function (d) {
     tooltip.transition()
-      .duration(200)
-      .style("opacity", 0)
-  }
+        .duration(200)
+        .style("opacity", 0)
+}
