c4b2b4e15131cab0b82a6f29e3f2bda321ee292b
JCBenR <jsieb81@gmail.com>
2021-08-05 14:56:57 -0600

    updates

--- newTest.js
+++ newTest.js
@@ -71,8 +71,8 @@ let covidData;
 var stateList = [];
 let selectedStates = [];
 let maxValue = 1_200_000;
-let width = 600;
-let height = 600;
+// let width = 600;
+// let height = 600;
 let month = months[0];
 let states;
 
@@ -124,10 +124,11 @@ Promise.all([
 
     var margin = { top: 20, right: 20, bottom: 70, left: 40 },
         width = 600 - margin.left - margin.right,
-        height = 300 - margin.top - margin.bottom;
+        height = 750 - margin.top - margin.bottom;
 
-    
-    var svg = d3.select("canvas")
+
+
+    var svg = d3.select("#canvas")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -136,33 +137,35 @@ Promise.all([
 
     var currYear = yearData.filter(obj => { return obj.m === month; })[0];
     var currYearData = sf[0];
+
     console.log(currYearData);
+
     var x = d3.scaleBand()
-        .domain(currYearData[0].map(function(d) { return d.data.ABBR;}))
+        .domain(currYearData[0].map(function (d) { return d.data.ABBR; }))
         .range([margin.left, width - margin.right])
 
     var y = d3.scaleLinear()
-        .domain([0, d3.max(currYearData, (d) => {  
-            return d3.max(d, (d) => { 
-                return parseInt(d[1]) + parseInt(d[0]); 
-            });  
+        .domain([0, d3.max(currYearData, (d) => {
+            return d3.max(d, (d) => {
+                return parseInt(d[1]) + parseInt(d[0]);
+            });
         })]).nice()
         .range([height - margin.bottom, margin.top])
 
     console.log("********");
-    console.log(d3.max(currYearData, (d) => {  
-            return d3.max(d, (d) => { 
-                return parseInt(d[1]) + parseInt(d[0]); 
-            });  
-        }));
+    console.log(d3.max(currYearData, (d) => {
+        return d3.max(d, (d) => {
+            return parseInt(d[1]) + parseInt(d[0]);
+        });
+    }));
 
     var xAxis = g => g
         .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x).tickFormat(i => sf[0][i].name).tickSizeOuter(0))
+        .call(d3.axisBottom(x).tickFormat(i => i).tickSizeOuter(0))
 
     var yAxis = g => g
         .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y).ticks(null, sf.format))
+        .call(d3.axisLeft(y).ticks(null, currYearData.format))
         .call(g => g.select(".domain").remove())
         .call(g => g.append("text")
             .attr("x", -margin.left)
@@ -171,7 +174,57 @@ Promise.all([
             .attr("text-anchor", "start")
             .text(sf.y));
 
-    
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    var groups = svg.selectAll("g.state")
+        .data(currYearData)
+        .enter().append("g")
+        .attr("class", "state")
+        .style("fill", function (d, i) { return colors[i]; });
+
+    console.log(">>>>>>>>>>");
+    console.log(typeof groups);
+
+    var rect = groups.selectAll("rect")
+        .data(function (d) { return d; })
+        .enter()
+        .append("rect")
+        .attr("x", function (d) { return x(d[0]); })
+        .attr("y", function (d) { return y(d[1] + d[0]); })
+        .attr("height", function (d) { return y(d[1]) - y(d[1] + d[0]); })
+        .attr("width", x.rangeBand())
+        .on("mouseover", function () { tooltip.style("display", null); })
+        .on("mouseout", function () { tooltip.style("display", "none"); })
+        .on("mousemove", function (d) {
+            var xPosition = d3.mouse(this)[0] - 15;
+            var yPosition = d3.mouse(this)[1] - 25;
+            tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+            tooltip.select("text").text(d[0]);
+        });
+
+    var tooltip = svg.append("g")
+        .attr("class", "tooltip")
+        .style("display", "none");
+
+    tooltip.append("rect")
+        .attr("width", 30)
+        .attr("height", 20)
+        .attr("fill", "white")
+        .style("opacity", 0.5);
+
+    tooltip.append("text")
+        .attr("x", 15)
+        .attr("dy", "1.2em")
+        .style("text-anchor", "middle")
+        .attr("font-size", "12px")
+        .attr("font-weight", "bold");
 })
     .catch((e) => {
         for (const x of Object.entries(e)) {
@@ -246,45 +299,45 @@ function handleClick(e, d) {
         stateData.x = canvasWidth * Math.random();
         stateData.y = canvasHeight * Math.random();
         selectedStates.push(stateData);
-        console.log(covidData.AK);
+        //console.log(covidData.AK);
     } else { //unselected
         selectedStates = selectedStates.filter(x => x.name != d);
     }
     console.log('clicked ', d);
     console.log(selectedStates)
-    updateGraph();
+    //updateGraph();
 }
 
-const updateGraph = () => {
-    console.log("updating...");
-    bars();
-}
+// const updateGraph = () => {
+//     console.log("updating...");
+//     bars();
+// }
 
-let canvas = d3.select("#canvas")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+// let canvas = d3.select("#canvas")
+//     .append("svg")
+//     .attr("width", width)
+//     .attr("height", height);
 
-let bars = () => canvas.selectAll("rect")
+// let bars = () => canvas.selectAll("rect")
 
-    .data(selectedStates)
-    .enter()
-    .append("rect")
-    .attr("width", 25)
-    .attr("height", 200)
-    .attr("fill", function (d) { return colorScale(d); }
-    )
+//     .data(selectedStates)
+//     .enter()
+//     .append("rect")
+//     .attr("width", 25)
+//     .attr("height", 200)
+//     .attr("fill", function (d) { return colorScale(d); }
+//     )
 
 
 const colorScale = d3.scaleLinear()
     .domain([0, maxValue])
     .range(['#0b661a', '#dbc70d', '#db330d'])
 
-const widthScale = d3.scaleLinear()
-    .domain([50, maxValue])
-    .range([0, width])
+// const widthScale = d3.scaleLinear()
+//     .domain([50, maxValue])
+//     .range([0, width]);
 
-const selectAll = (e) => {
+function selectAllStates(e) {
     if (e.className === "selectAll") {
         selectedStates = [...covidData];
         let sl = document.getElementById("stateList").getElementsByTagName("p");
@@ -305,37 +358,37 @@ const selectAll = (e) => {
     }
 }
 
-var tooltip = d3.select("#canvas")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("background-color", "white")
-    .style("border", "solid")
-    .style("border-width", "1px")
-    .style("border-radius", "5px")
-    .style("padding", "10px")
+// var tooltip = d3.select("#canvas")
+//     .append("div")
+//     .style("opacity", 0)
+//     .attr("class", "tooltip")
+//     .style("background-color", "white")
+//     .style("border", "solid")
+//     .style("border-width", "1px")
+//     .style("border-radius", "5px")
+//     .style("padding", "10px")
 
 
-var mouseover = function (d) {
-    tooltip.style("opacity", 1)
-}
+// var mouseover = function (d) {
+//     tooltip.style("opacity", 1)
+// }
 
-var mousemove = function (d) {
-    tooltip.html(`State: ${d.State} <br/>
-                Population: ${d.Population} <br/>
-                Cases: ${d.data[month][0]} <br/>
-                UnEmp Rate: ${d.data[month][1]}`
-        // "State: " + d.State + <br/> +
-        //     "Population: " + d.Population + <br/> +
-        //     "Cases: " + d.data[month][0] + <br/> +
-        //     "UnEmp Rate: " + d.data[month][1]     
-    )
-        .style("left", (d3.mouse(this)[0] + 90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
-        .style("top", (d3.mouse(this)[1]) + "px")
-}
+// var mousemove = function (d) {
+//     tooltip.html(`State: ${d.State} <br/>
+//                 Population: ${d.Population} <br/>
+//                 Cases: ${d.data[month][0]} <br/>
+//                 UnEmp Rate: ${d.data[month][1]}`
+//         // "State: " + d.State + <br/> +
+//         //     "Population: " + d.Population + <br/> +
+//         //     "Cases: " + d.data[month][0] + <br/> +
+//         //     "UnEmp Rate: " + d.data[month][1]     
+//     )
+//         .style("left", (d3.mouse(this)[0] + 90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+//         .style("top", (d3.mouse(this)[1]) + "px")
+// }
 
-var mouseleave = function (d) {
-    tooltip.transition()
-        .duration(200)
-        .style("opacity", 0)
-}
+// var mouseleave = function (d) {
+//     tooltip.transition()
+//         .duration(200)
+//         .style("opacity", 0)
+// }
